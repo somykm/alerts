44489c5fec8193013b8f8b48e704dfaaf54aabe8
https://github.com/somykm <sk43@hood.edu>
2025-06-12 23:53:31 -0400

    added PersonInfoDTO, Data Transfer Object, to tailor respond object for this endpoint http://localhost:8080/personInfolastName=<lastName>

--- alerts/src/main/java/com/safetynet/alerts/domain/PersonInfoDTO.java
+++ alerts/src/main/java/com/safetynet/alerts/domain/PersonInfoDTO.java
@@ -1,4 +1,14 @@
 package com.safetynet.alerts.domain;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class PersonInfoDTO {
+    private String firstName;
+    private String address;
+    private String age;
+    private String email;
+    private List<String> medicalHistory;
 }
